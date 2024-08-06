4de93411c66cff7c5966fb4bf7f6aae23c0d9e05
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-08-06 16:26:00 +0200

    first

---
+++ tp_vuejs/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <HelloWorld :user="user"/>
+  
+    <div
+    v-if="user.age < 15"
+    >
+     mon age 
+    </div
+    
+    v-else
+    >
+     else
+
+    <div>
+
+
+    </div>
+
+  <input type="text" v-model="user.name" v-on:input="display">
+  <input type="text" v-model="user.age">
+  </template>
+  
+  <script setup>
+  import HelloWorld from '@/components/HelloWorld.vue';
+  import { reactive } from "vue";
+  const user = reactive ({
+   name: 'NordCoders',
+   age: 30,
+  });
+  
+  const display = () => {
+    console.log(user.name ,user.age)
+  }
+   
+  display()
+  
+  </script>
+  
+  <style>
+  
+  </style>
+  
---
+++ tp_vuejs/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>Hello World </h1>
+    <p>{{ props.user.name }} {{ props.user.age }}</p>
+  </div>
+</template>
+
+<script setup>
+ const props = defineProps(['user']);
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
